a58f5fe824f41bff599c199cdcce4aa125155a83
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-08 16:14:31 +0200

    The first challenge from the sixth level

---
+++ TheVariableShop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+byte byteNumber = 1;
+sbyte sbyteNumber = 2;
+short shortNumber = 3;
+ushort ushortNumber = 4;
+int intNumber = 5;
+uint uintNumber = 6;
+long longNumber = 7;
+ulong ulongNumber = 8;
+float floatNumber = 9.0f;
+double doubleNumber = 10;
+decimal decimalNumber = 11.0m;
+char character = 'a';
+string name = "Monty";
+bool isExpected = false;
+
+Console.WriteLine($"{byteNumber} {sbyteNumber} {shortNumber} {shortNumber} {ushortNumber} {intNumber} {uintNumber} {longNumber} {ulongNumber}");
+Console.WriteLine($"{floatNumber} {doubleNumber} {decimalNumber} {character} {name} {isExpected}");
