c6eba6c6994a1b411fa790774e7d2681c2aa48f2
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-08 14:04:04 +0200

    The third challenge from the third level

---
+++ TheMakingOfAProgrammer.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+Console.WriteLine("What are you doing this morning?");
+Console.WriteLine("It's a birth.");
+Console.WriteLine("And what sort of thing is that?");
+Console.WriteLine("Well, that's where we take a new baby out of a lady's tummy.");
+Console.WriteLine("Wonderful what we can do nowadays!");
