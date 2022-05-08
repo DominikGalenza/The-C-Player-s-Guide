8bfa92a419157fed4e94b62735d4778f35df8c4f
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-08 14:48:44 +0200

    The challenge from the fourth level

---
+++ TheThingNamer3000.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+Console.WriteLine("What kind of thing are we talking about?");
+// Kind of thing
+string a = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// Description
+string b = Console.ReadLine();
+/* Text */
+string c = "of Doom";
+/* Number */
+string d = "3000";
+
+Console.WriteLine($"The {b} {a} {c} {d}!");
