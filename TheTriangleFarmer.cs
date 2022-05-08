185ceec10a069901146ba118a81791eb6787e6cc
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-08 16:44:37 +0200

    The first challenge from the seventh level

---
+++ TheTriangleFarmer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+Console.WriteLine("Write the triangle's base:");
+float triangleBase = Convert.ToSingle(Console.ReadLine());
+
+Console.WriteLine("Write the triangle's height:");
+float triangleHeight = Convert.ToSingle(Console.ReadLine());
+
+float triangleArea = (triangleBase * triangleHeight) / 2;
+Console.WriteLine($"The triangle's area is {triangleArea}");
