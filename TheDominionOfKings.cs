d6418a5082b1a9af92b4499f532ab09e767aae6f
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-08 18:14:59 +0200

    The third challenge from the seventh level

---
+++ TheDominionOfKings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.WriteLine("How many provinces do you have?");
+int numberOfProvinces = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many duchies do you have?");
+int numberOfDuchies = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("How many estates do you have?");
+int numberOfEstates = Convert.ToInt32(Console.ReadLine());
+
+int numberOfPoints = numberOfProvinces * 6 + numberOfDuchies * 3 + numberOfEstates;
+Console.WriteLine($"You have {numberOfPoints} points");
