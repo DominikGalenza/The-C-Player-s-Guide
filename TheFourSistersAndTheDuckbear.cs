0879c247fcfbbed6907a56f7840d67dc3cbffb5a
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-08 17:00:16 +0200

    The second challenge from the seventh level

---
+++ TheFourSistersAndTheDuckbear.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+Console.WriteLine("How many chocolate eggs did the sisters gather today?");
+
+int eggsCount = Convert.ToInt32(Console.ReadLine());
+int eggsForSisters = eggsCount / 4;
+int eggsForDuckbear = eggsCount % 4;
+
+Console.WriteLine($"Sisters got {eggsForSisters} eggs, and Duckbear got {eggsForDuckbear} eggs.");
