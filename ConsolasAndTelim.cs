fb54d1b5411deaefc431afb8dda2f3d399a305f7
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-08 14:13:25 +0200

    The fourth challenge from the third level

---
+++ ConsolasAndTelim.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine("Bread is ready.");
+Console.WriteLine("Who is the bread for?");
+string passedName = Console.ReadLine();
+Console.WriteLine($"Noted: {passedName} got bread.");
