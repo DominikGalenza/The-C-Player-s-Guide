71958d60848c0103fec893e8aab9d584f9be0852
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-09 19:23:42 +0200

    The first challenge from the ninth level

---
+++ RepairingTheClocktower.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.WriteLine("Write a number:");
+int passedNumber = Convert.ToInt32(Console.ReadLine());
+
+if (passedNumber % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}
