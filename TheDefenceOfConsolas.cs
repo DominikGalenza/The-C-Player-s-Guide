37da1dc54476f9c7e6b0446d8f39dab4dde396d7
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-09 18:54:28 +0200

    The challenge from the eighth chapter

---
+++ TheDefenceOfConsolas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+Console.Title = "Defense of Consolas";
+
+Console.Write("Target Row? ");
+int targetRow = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Target Column? ");
+int targetColumn = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Deploy To:");
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine($"({targetRow}, {targetColumn - 1})");
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine($"({targetRow - 1}, {targetColumn})");
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($"({targetRow}, {targetColumn + 1})");
+
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine($"({targetRow + 1}, {targetColumn})");
+
+Console.Beep();
