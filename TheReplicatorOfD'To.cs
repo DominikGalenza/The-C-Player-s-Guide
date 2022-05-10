b9270bf1dac5f8650a5b558a8123dd71f7dd70a6
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-10 19:58:42 +0200

    The first challenge from the 12th level

---
+++ TheReplicatorOfD'To.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+int[] numbers = new int[5];
+
+for (int i = 0; i < numbers.Length; i++)
+{
+    Console.WriteLine("Write a number:");
+    int number = Convert.ToInt32(Console.ReadLine());
+    numbers[i] = number;
+}
+
+int[] numbersCopy = new int[5];
+
+for (int i = 0; i < numbersCopy.Length; i++)
+{
+    numbersCopy[i] = numbers[i];
+    Console.WriteLine($"Number: {numbers[i]} and its copy: {numbersCopy[i]}.");
+}
