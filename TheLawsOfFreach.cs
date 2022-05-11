6b1d845fb0a45787998d52730a85dd49e7f0fa0b
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-11 19:15:33 +0200

    The second challenge from the 12th level

---
+++ TheLawsOfFreach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int currentMinimum = int.MaxValue;
+int total = 0;
+
+foreach (int i in array)
+{
+    if (i < currentMinimum)
+    {
+        currentMinimum = array[i];
+    }
+}
+
+Console.WriteLine(currentMinimum);
+
+foreach (int i in array)
+{
+    total += i;
+}
+
+float average = (float)total / array.Length;
+Console.WriteLine(average);
