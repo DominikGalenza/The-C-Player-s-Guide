628e89ca58deba7874c94e4ac2e540c48f9a33f7
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-11 19:55:26 +0200

    The first challenge from the 13th level

---
+++ TakingANumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+AskForNumber("Write a random number");
+
+AskForNumberInRange("Write a number in range from 25 to 49", 25, 49);
+
+int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int passedNumber = Convert.ToInt32(Console.ReadLine());
+    return passedNumber;
+}
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        int passedNumber = AskForNumber(text);
+        if (passedNumber >= min && passedNumber <= max)
+        {
+            return passedNumber;
+        }
+    }
+}
