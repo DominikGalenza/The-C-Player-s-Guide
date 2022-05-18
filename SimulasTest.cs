e51acc5059af4c0a0101423b5b827ab006e7b5be
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-18 19:24:42 +0200

    The challenge from the 16th level

---
+++ SimulasTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+ChestState state = ChestState.Locked;
+
+while (true)
+{
+    Console.Write($"The chest is {state}. What do you want to do? ");
+    string action = Console.ReadLine();
+
+    if (state == ChestState.Locked && action == "unlock")
+    {
+        state = ChestState.Closed;
+    }
+    else if (state == ChestState.Closed && action == "open")
+    {
+        state = ChestState.Open;
+    }
+    else if (state == ChestState.Open && action == "close")
+    {
+        state = ChestState.Closed;
+    }
+    else if (state == ChestState.Closed && action == "lock")
+    {
+        state = ChestState.Locked;
+    }
+}
+
+enum ChestState { Locked, Closed, Open };
