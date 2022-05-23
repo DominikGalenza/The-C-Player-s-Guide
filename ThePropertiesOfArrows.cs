f9ec452b17d04e89bc253ab9501c8d1aef0d7bb8
Dominik Gałenza <dominik.galenza@gmail.com>
2022-05-23 19:44:26 +0200

    The challenge from the 20th level

---
+++ ThePropertiesOfArrows.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+using System;
+
+Arrow arrow = GetArrow();
+Console.WriteLine($"Your chosen arrow costs {arrow.GetCost()} gold.");
+
+Arrow GetArrow()
+{
+    Arrowhead arrowhead = GetArrowhead();
+    Fletching fletching = GetFletching();
+    float shaftLength = GetShaftLength();
+
+    return new Arrow(arrowhead, fletching, shaftLength);
+}
+
+Arrowhead GetArrowhead()
+{
+    Console.WriteLine("Choose arrowhead material from steel, wood or obsidian:");
+    string arrowhead = Console.ReadLine();
+
+    return arrowhead switch
+    {
+        "steel" => Arrowhead.Steel,
+        "wood" => Arrowhead.Wood,
+        "obsidian" => Arrowhead.Obsidian
+    };
+}
+
+Fletching GetFletching()
+{
+    Console.WriteLine("Choose fletching material from plastic, turkey feathers or goose feathers:");
+    string fletching = Console.ReadLine();
+
+    return fletching switch
+    {
+        "plastic" => Fletching.Plastic,
+        "turkey feathers" => Fletching.TurkeyFeathers,
+        "goose feathers" => Fletching.GooseFeathers
+    };
+}
+
+float GetShaftLength()
+{
+    float shaftLength = 0;
+
+    while (shaftLength < 60 || shaftLength > 100)
+    {
+        Console.WriteLine("Choose shaft length from 60 and 100 cm:");
+        shaftLength = Convert.ToSingle(Console.ReadLine());
+    }
+
+    return shaftLength;
+}
+
+public class Arrow
+{
+    public Arrowhead Arrowhead { get; }
+    private Fletching Fletching { get; }
+    private float ShaftLength { get; }
+
+    public Arrow(Arrowhead arrowhead, Fletching fletching, float shaftLength)
+    {
+        Arrowhead = arrowhead;
+        Fletching = fletching;
+        ShaftLength = shaftLength;
+    }
+
+    public float GetCost()
+    {
+        float arrowheadCost = Arrowhead switch
+        {
+            Arrowhead.Steel => 10,
+            Arrowhead.Wood => 3,
+            Arrowhead.Obsidian => 5
+        };
+
+        float fletchingCost = Fletching switch
+        {
+            Fletching.Plastic => 10,
+            Fletching.TurkeyFeathers => 5,
+            Fletching.GooseFeathers => 3
+        };
+
+        float shaftCost = ShaftLength * 0.05f;
+
+        return arrowheadCost + fletchingCost + shaftCost;
+    }
+}
+
+public enum Arrowhead { Steel, Wood, Obsidian };
+public enum Fletching { Plastic, TurkeyFeathers, GooseFeathers };
